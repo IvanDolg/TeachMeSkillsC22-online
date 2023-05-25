3b487581e626553aa622fb8f6b8f925587d0e489
Ivan Dauhalaptseu <ivandolgolaptev@gmail.com>
2023-05-25 16:48:18 +0300

    feat: add 1 task answer

--- src/HomeWork4.java
+++ src/HomeWork4.java
@@ -1,2 +1,22 @@
 public class HomeWork4 {
+    public static void main(String[] args) {
+        System.out.println(getSeson(5));
+    }
+
+    // 1 task
+    // The names of the time of the year by the number of the month.
+    public static String getSeson(int month){
+        switch (month){
+            case 1, 2, 12:
+                return "This is a Winter";
+            case 3, 4, 5:
+                return "This is a Spring";
+            case 6, 7, 8:
+                return "This is a Summer";
+            case 9, 10, 11:
+                return "This is a Autumn";
+            default:
+                return "There is no such month in the year";
+        }
+    }
 }
