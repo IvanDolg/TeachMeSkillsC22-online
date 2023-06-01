753c5f0ef9cc0a12d95eff2933194cf457e88050
Ivan Dauhalaptseu <ivandolgolaptev@gmail.com>
2023-06-02 00:45:07 +0300

    init: add new branch

---
+++ src/HomeWork6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HomeWork6 {
+    public static void main(String[] args) {
+
+    }
+    public static Scanner scannerIn(){
+        return new Scanner(System.in);
+    }
+}
