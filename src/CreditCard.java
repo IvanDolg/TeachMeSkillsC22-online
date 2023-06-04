4c354a9f601e2fa1742c075910b7e5ffddcb3d10
Ivan Dauhalaptseu <ivandolgolaptev@gmail.com>
2023-06-04 15:01:32 +0300

    feat: add method sum for loan

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -11,18 +11,23 @@ public class CreditCard {
         return new Scanner(System.in);
     }
     public static void sumForLoan(){
-        System.out.print("Enter loan amount: ");
-        int loanAmount = scanner().nextInt();
+        System.out.print("Enter the amount of debt: ");
+        double amountOfDebt = scanner().nextDouble();
 
-        System.out.print("Enter the interest rate (month): ");
+        System.out.print("Enter the interest rate (as a percent): ");
         double interestRate = scanner().nextDouble();
-        interestRate = interestRate/100/12;
+        interestRate /= 100;
 
-        System.out.print("Enter maturity date: ");
-        double maturityDate = scanner().nextDouble();
+        System.out.print("Enter the period for which the amount was taken (in days): ");
+        int period = scanner().nextInt();
 
-        System.out.print("The amount of the monthly payment: ");
-        double amountPayment = loanAmount *  (interestRate + (interestRate/(Math.pow(1 + interestRate, maturityDate) - 1)));
-        System.out.print(amountPayment);
+        System.out.print("The amount of interest for the reporting period: ");
+        double amountOfInterest = ((amountOfDebt * interestRate) / 365) * period;
+        System.out.print(Math.round(amountOfInterest) + "\n");
+
+        System.out.print("Total loan amount: ");
+        double totalAmount = (amountOfInterest * period) + amountOfDebt;
+        System.out.print(Math.round(totalAmount));
+
     }
 }
