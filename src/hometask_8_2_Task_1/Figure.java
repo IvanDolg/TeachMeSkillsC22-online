0f14f23e697b1e2feaeb0ee7612a23a78624f66b
Ivan Dauhalaptseu <ivandolgolaptev@gmail.com>
2023-06-10 15:39:50 +0300

    feat: add 8_2 1 task

---
+++ src/hometask_8_2_Task_1/Figure.java
@@ -0,0 +1,6 @@
+package hometask_8_2_Task_1;
+public abstract class Figure {
+    public abstract double getSquare();
+    public abstract double getPerimeter();
+    public abstract String toString();
+}
--- src/hometask_8_2_Task_1/Main.java
+++ src/hometask_8_2_Task_1/Main.java
@@ -1,13 +1,23 @@
 package hometask_8_2_Task_1;
-
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        Figure[] figures = new Figure[]{
+                new Triangle(3,4,5),
+                new Triangle(13, 14, 15),
+                new Rectangle(4, 5),
+                new Rectangle(6, 2),
+                new Circle(10)
+        };
+        System.out.println(Arrays.toString(figures));
+        System.out.println(getSum(figures));
     }
-    //Написать иерархию классов «Фигуры».
-    //Фигура -> Треугольник -> Прямоугольник -> Круг.
-    //Реализовать ф-ю подсчета площади для каждого типа фигуры и подсчет
-    //периметра.
-    //Создать массив из 5 фигур.
-    //Вывести на экран сумму периметра всех фигур в массиве.
+    public static double getSum(Figure[] figures){
+        double sum = 0;
+        for (Figure figure : figures){
+            sum += figure.getPerimeter();
+        }
+        return sum;
+    }
 }
