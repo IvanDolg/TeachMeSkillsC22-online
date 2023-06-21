f0432b142365d0efe46a207e986454b71f837734
Ivan <ivandolgolaptev@gmail.com>
2023-06-21 17:59:27 +0300

    init: add new branch homework_9

--- src/hometask_8_2_Task_1/Figure.java
+++
@@ -1,6 +0,0 @@
-package hometask_8_2_Task_1;
-public abstract class Figure {
-    public abstract double getSquare();
-    public abstract double getPerimeter();
-    public abstract String toString();
-}
--- src/hometask_8_2_Task_2/Data.java
+++
@@ -1,4 +0,0 @@
-package hometask_8_2_Task_2;
-public interface Data {
-    void getPosition();
-}
