bc65a25ebee6df0dc585a20b60fbddada1d18a2d
Ivan <ivandolgolaptev@gmail.com>
2023-06-28 18:35:04 +0300

    feat: add new task

--- src/Task_4/NewLine.java
+++ src/Task_4/NewLine.java
@@ -4,11 +4,21 @@ package Task_4;
  затем в каждом элементе получившегося массива подсчитывать
  число разных символов и искать минимум (держать минимальное
  слово в какой-то переменной и сравнивать) */
+
+/*               it doesn't work!!!
+                I'll think about it         */
+
 public class NewLine {
     public static void minimalWordDifference(String line){
         String[] parts = line.split(" ");
 
         for (int i = 0; i < parts.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < parts.length; j++) {
+                if (parts[i].charAt(j) == parts[i].charAt(j++)){
+                    counter ++;
+                }
+            }
         }
     }
  }
