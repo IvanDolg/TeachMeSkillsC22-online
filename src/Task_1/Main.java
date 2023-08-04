2b49b51c97193ec4dbcc9e3e2f870200c0b7b3c1
Ivan <ivandolgolaptev@gmail.com>
2023-08-04 20:49:54 +0300

    аÑfeat: add new Task_1

--- src/Task_1/Main.java
+++ src/Task_1/Main.java
@@ -6,8 +6,24 @@ package Task_1;
         помощью регулярного выражения и выводить результат проверки на
         экран.*/
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter id address: " );
+        String ipAddress = scanner.nextLine();
 
+
+        String regexp = "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$";
+
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(ipAddress);
+
+       if (matcher.find()){
+           System.out.println(pattern.matcher(ipAddress).matches());
+       } else System.out.println("Ip address does not fit the condition");
     }
 }
